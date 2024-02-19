f38f1019341ad31be9709c6648042575aa43b39d
Dauris <edauris@ford.com>
2024-02-19 13:04:41 +0100

    update bash file

--- Dockerfile
+++ Dockerfile
@@ -24,20 +24,7 @@ RUN yum -y install \
 
 COPY bash.sh /bash.sh
 RUN chmod +x /bash.sh
-
-##old process##
-#docker build --platform linux/amd64 -t my-centos-image .
-#docker run --privileged -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro my-centos-image
-#docker exec -it [container-id] /bin/bash
-#test docker systemctl status
-#docker cp bash.sh 181ccb6fff6cb037516196e0b938d9c85095755f0704375569c7fd035bd60066:home
-#Successfully copied 3.58kB to 181ccb6fff6cb037516196e0b938d9c85095755f0704375569c7fd035bd60066:home
-#on remote, navigate to home
-#chmod +x bash.sh
-#$bash /home/bash.sh
-#to verify
-#sudo systemctl start httpd
-#Failed to get D-Bus connection: Operation not permittedç
+RUN /bash.sh
 
 ##new process##
 #docker build --platform linux/amd64 -t my-centos-image .
--- bash.sh
+++ bash.sh
@@ -5,7 +5,7 @@ HTTPD_VERSION="2.4.58"
 
 # Install necessary development tools and dependencies
 yum groupinstall -y "Development Tools"
-yum install -y rpm-build wget
+yum install -y rpm-build wget autoconf libtool doxygen openssl-devel libxml2-devel lua-devel openldap-devel
 
 # Create the rpmbuild directories
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
@@ -13,9 +13,28 @@ mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 # Download the source tarball of the specified version of httpd
 wget -P ~/rpmbuild/SOURCES "https://downloads.apache.org/httpd/httpd-$HTTPD_VERSION.tar.gz"
 
-# Download the additional necessary files
-wget -P ~/rpmbuild/SOURCES "https://downloads.apache.org/apr/apr-1.7.4.tar.gz"
-wget -P ~/rpmbuild/SOURCES "https://downloads.apache.org/apr/apr-util-1.6.3.tar.gz"
+# Check if APR and APR-util are installed
+if [ ! -f /usr/lib64/libapr-1.so ] || [ ! -f /usr/lib64/libaprutil-1.so ]; then
+    echo "APR or APR-util not found in /usr/lib64, attempting to build from source."
+    # Download the additional necessary files
+    wget -P ~/rpmbuild/SOURCES "https://downloads.apache.org/apr/apr-1.7.4.tar.gz"
+    wget -P ~/rpmbuild/SOURCES "https://downloads.apache.org/apr/apr-util-1.6.3.tar.gz"
+
+    # Build and install APR and APR-util from source
+    rpmbuild -tb --clean ~/rpmbuild/SOURCES/apr-1.7.4.tar.gz
+    rpmbuild -tb --clean ~/rpmbuild/SOURCES/apr-util-1.6.3.tar.gz
+
+    # Install the built APR and apr-devel packages
+    rpm -Uvh ~/rpmbuild/RPMS/x86_64/apr-1.7.4-*.x86_64.rpm ~/rpmbuild/RPMS/x86_64/apr-devel-1.7.4-*.x86_64.rpm
+fi
+
+# Download and rebuild distcache (adjust the version as necessary)
+wget https://archive.fedoraproject.org/pub/archive/fedora/linux/releases/18/Everything/source/SRPMS/d/distcache-1.4.5-23.src.rpm -P ~/rpmbuild/SOURCES
+rpmbuild --rebuild --clean ~/rpmbuild/SOURCES/distcache-1.4.5-23.src.rpm
+
+# Install the built distcache and distcache-devel packages
+cd ~/rpmbuild/RPMS/x86_64
+rpm -Uvh distcache-1.4.5-23.x86_64.rpm distcache-devel-1.4.5-23.x86_64.rpm
 
 # Create a basic spec file for RPM
 cat <<EOT > ~/rpmbuild/SPECS/httpd.spec
@@ -27,23 +46,23 @@ Summary:        Apache HTTP Server
 License:        ASL 2.0
 URL:            https://httpd.apache.org/
 Source0:        https://downloads.apache.org/httpd/httpd-%{version}.tar.gz
-Source1:        https://downloads.apache.org/apr/apr-1.7.4.tar.gz
-Source2:        https://downloads.apache.org/apr/apr-util-1.6.3.tar.gz
 
-BuildRequires:  gcc, make, pcre-devel, expat-devel, libxml2-devel
+BuildRequires:  gcc, make, pcre-devel, expat-devel, libxml2-devel, openssl-devel, openldap-devel, lua-devel, apr-devel, apr-util-devel
 
 %description
 The Apache HTTP Server, a robust, commercial-grade, featureful, and freely-available source code implementation of an HTTP (Web) server.
 
 %prep
 %setup -q -n httpd-%{version}
-tar xzf %{SOURCE1}
-tar xzf %{SOURCE2}
-mv apr-1.7.4 srclib/apr
-mv apr-util-1.6.3 srclib/apr-util
 
 %build
-./configure --prefix=/usr/local/apache2 --with-included-apr
+./configure --prefix=/usr/local/apache2 \
+            --with-included-apr \
+            --enable-ssl \
+            --enable-so \
+            --enable-authnz-ldap \
+            --enable-lua \
+            --enable-proxy-html
 make %{?_smp_mflags}
 
 %install
@@ -53,7 +72,7 @@ make install DESTDIR=%{buildroot}
 /usr/local/apache2
 
 %changelog
-* Thu Jan 23 2024 Emma Dauris <emma.dauris@netcentric.biz> - %{version}-1
+* Mon Feb 19 2024 Emma Dauris <emma.dauris@netcentric.biz> - %{version}-1
 - First build
 EOT
 
